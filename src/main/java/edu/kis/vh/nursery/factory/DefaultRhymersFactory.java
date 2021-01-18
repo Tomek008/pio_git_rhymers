f5c75b9a9878601fda4417aacc9cced6d36efcc0
Tomek008 <tomasz.nykiel123@gmail.com>
2021-01-18 10:20:48 +0100

    Usunięcie pliku klasy FifoRhymers

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -1,9 +1,27 @@
 package edu.kis.vh.nursery.factory;
 
 import edu.kis.vh.nursery.DefaultCountingOutRhymer;
-import edu.kis.vh.nursery.FifoRhymer;
 import edu.kis.vh.nursery.HanoiRhymer;
 import edu.kis.vh.nursery.factory.RhymersFactory;
+
+class FifoRhymer extends DefaultCountingOutRhymer {
+
+	public DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
+
+	@Override
+	public int countOut() {
+		while (!callCheck())
+			temp.countIn(super.countOut());
+
+		int ret = temp.countOut();
+
+		while (!temp.callCheck())
+			countIn(temp.countOut());
+
+		return ret;
+		//alt + ← oraz alt + → przełączają się pomiędzy plikami w projekcie
+	}
+}
 
 public class DefaultRhymersFactory implements RhymersFactory {
 
